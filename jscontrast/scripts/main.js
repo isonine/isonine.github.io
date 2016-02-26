f240523391e7f922850087093cf9987d8fff304e
isonine <timolitt@gmail.com>
2016-02-26 11:27:02 -0800

    ia[jsjdflkajdsf;adsf

--- jscontrast/scripts/main.js
+++ jscontrast/scripts/main.js
@@ -1,7 +1,10 @@
 
 var splitCSV = function (csv){
   var partsOfStr = csv.split(',');
-
+  if (partsOfStr[0]>255||partsOfStr[1]>255||partsOfStr[2]>255){
+    alert("Invalid input");
+    window.location.reload();
+  }
   return partsOfStr;
 }
 
@@ -17,6 +20,8 @@ function testResults (form) {
     }else{
       alert("it didn't work :(");
     }
+    htmlColor = RGB2HTML(split[0],split[1],split[2]);
+    console.log(htmlColor);
 
 }
 
@@ -26,4 +31,10 @@ var colourIsLight = function (r, g, b) {
   var a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
   console.log(a);
   return (a < 0.5);
+}
+
+function RGB2HTML(red, green, blue)
+{
+    var decColor =0x1000000+ blue + 0x100 * green + 0x10000 *red ;
+    return '#'+decColor.toString(16).substr(1);
 }
